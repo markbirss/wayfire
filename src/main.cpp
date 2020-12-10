4d10670b88b7417b9e6c20871b1d229df4f3a88e
Ilia Bozhinov <ammen99@gmail.com>
2020-12-10 23:21:55 +0100

    fixup! fixup! fixup! fixup! fixup! config: move everything to a dedicated config backend plugin

--- src/main.cpp
+++ src/main.cpp
@@ -31,7 +31,8 @@ static void print_help()
     std::cout << "Wayfire " << WAYFIRE_VERSION << std::endl;
     std::cout << "Usage: wayfire [OPTION]...\n" << std::endl;
     std::cout << " -c,  --config            specify config file to use" << std::endl;
-    std::cout << " -B,  --config-backend    specify config backend to use" << std::endl;
+    std::cout << " -B,  --config-backend    specify config backend to use" <<
+    std::endl;
     std::cout << " -h,  --help              print this help" << std::endl;
     std::cout << " -d,  --debug             enable debug logging" << std::endl;
     std::cout <<
